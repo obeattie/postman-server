3b1ecd746c5c417ae821b6062dde435ac9269118
Oliver Beattie <oliver@obeattie.com>
2011-03-15 08:46:11 -0400

    Trying out new logging

--- realtime.js
+++ realtime.js
@@ -1,14 +1,15 @@
 var redis = require('redis'),
     _ = require('underscore'),
     sender = redis.createClient(),
-    receiver = redis.createClient();
+    receiver = redis.createClient(),
+    logger = require('node-logger').logger(module);
 
 // The reactor is responsible for sending/receiving all messages
 var Reactor = {
     subscriptions: {},
     
     subscribe: function(channel, cb){
-        console.log('realtime.Reactor:subscribe ', channel);
+        logger.debug('invocation', channel);
         if (!(channel in this.subscriptions)) {
             this.subscriptions[channel] = [];
             receiver.subscribe(channel);
@@ -17,28 +18,30 @@ var Reactor = {
     },
     
     unsubscribe: function(channel, cb){
-        console.log('realtime.Reactor:unsubscribe ', channel);
+        logger.debug('invocation', channel);
         this.subscriptions[channel] = (this.subscriptions[channel] || []);
         this.subscriptions[channel] = _.without(this.subscriptions[channel], cb);
     },
     
     receive: function(channel, value){
-        console.log('realtime.Reactor:receive ', channel, value);
+        logger.debug('invocation', channel, value);
         _.each((this.subscriptions[channel] || []), function(cb){
             cb(value);
         });
     },
     
     send: function(channel, value){
-        console.log('realtime.Reactor:publish ', channel, value);
+        logger.debug('invocation', channel, value);
         return sender.publish(channel, value);
     },
     
     listen: function(){
+        logger.debug('invocation');
         receiver.on('message', this.receive);
     },
     
     silence: function(sessionId){
+        logger.debug('invocation', sessionId);
         console.log('realtime.Reactor:silencing ' + sessionId);
         _.each(this.subscriptions, function(value, key){
             console.log('    before: ', this.subscriptions[key]);
