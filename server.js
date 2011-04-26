8841edd3950995a5063c918e7b66584332f40867
Oliver Beattie <oliver@obeattie.com>
2011-04-26 11:34:14 +0100

    Use different logfile/pidfile locations

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var cluster = require('cluster'),
     app = require('./app');
 
 cluster('./app')
-    .use(cluster.logger('/var/log/postman-server'))
+    .use(cluster.logger('/var/log/postman-server.dev'))
     .use(cluster.stats())
-    .use(cluster.pidfiles('/var/run/node'))
+    .use(cluster.pidfiles('/var/run/node.dev'))
     .use(cluster.cli())
     .use(exception({to: 'webmaster@emberb0x.com'}))
     .listen(8088);
